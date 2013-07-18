050a49f39b006fd1b6e1f35d4d11eb260aa36dd0
maxired <github@maxired.fr>
2013-07-18 23:37:14 +0200

    regactor with server

---
+++ index.js
@@ -0,0 +1,95 @@
+var express = require("express"),
+	crypto = require('crypto');
+
+
+var svg = require('svg2png');
+
+var app = express();
+app.use(express.logger());
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+
+app.use(express.static('public'));
+var fs = require('fs');
+
+function hash(content) {
+	var shasum = crypto.createHash('sha1');
+	shasum.update(content, 'utf-8');
+	return shasum.digest("hex");
+}
+
+
+app.post('/image', function(req, res) {
+
+	console.log(req);
+	var hashed = hash(req.body.content);
+
+	collection.findOne({
+		_id: hashed
+	}, function(err, doc) {
+		if (doc) {
+			return res.send(doc);
+		};
+
+		collection.insert({
+			_id: hashed,
+			content: req.body.content
+		}, function(err, docs) {
+			if (err || !docs || docs.length != 1) {
+				return res.send(500, "oups something went wrong, please try again");
+			}
+			return res.send(docs[0]);
+		})
+	})
+})
+
+app.get('/image/:id', function(req, res) {
+
+	collection.findOne({
+		_id: req.params.id
+	}, function(err, doc) {
+
+		if (err) {
+			return res.send(err);
+		}
+		if (!doc || !doc.content) {
+			return res.send(404, {
+				error: "document not found"
+			});
+		}
+
+
+		var svgFileName = +new Date() + '' + Math.random();
+		pngFileName = svgFileName + ".png";
+		svgFileName += '.svg';
+
+		var svgPathName = path.join('/', 'tmp', svgFileName);
+		var pngPathName = path.join('/', 'tmp', pngFileName);
+
+		fs.writeFile(svgPathName, doc.svg, function(err) {
+			if (err) {
+				return res.send(err)
+			};
+			svg(svgPathName, pngPathName, function(err) {
+				if (err) {
+					return res.send(err)
+				};
+				res.sendFile(pngPathName);
+			});
+		})
+	})
+});
+;
+
+var MongoClient = require('mongodb').MongoClient;
+var mongoURL = process.env.MONGOHQ_URL || "mongodb://127.0.0.1:27017/tcl";
+var collection;
+MongoClient.connect(mongoURL, function(err, db) {
+	if (err) throw err;
+	collection = db.collection('images');
+
+	var port = process.env.PORT || 5000;
+	app.listen(port, function() {
+		console.log("Listening on " + port);
+	})
+})
